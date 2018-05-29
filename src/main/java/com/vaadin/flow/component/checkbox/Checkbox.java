492dde1a28edf48145922a3e8e75ce2e306391c8
Matti Tahvonen <matti@vaadin.com>
2018-05-29 15:40:37 +0300

    Trying to reformulate the javadoc comment so that it would help end users

--- src/main/java/com/vaadin/flow/component/checkbox/Checkbox.java
+++ src/main/java/com/vaadin/flow/component/checkbox/Checkbox.java
@@ -124,7 +124,9 @@ public class Checkbox extends GeneratedVaadinCheckbox<Checkbox, Boolean>
      * Set the current label text of this checkbox with HTML
      * formatting.
      *
-     * <p>Note, the given html content is not sanitized by the framework.</p>
+     * <p>XSS vulnerability warning: the given HTML is rendered
+     * in the browser as is and the developer is responsible
+     * for ensuring no harmful HTML is used.</p>
      *
      * @param htmlContent
      *            the label html to set
